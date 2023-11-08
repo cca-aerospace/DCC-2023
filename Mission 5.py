89619dd3b09574f88ba813878cb25396324a8182
Tan Vu <67347281+tanvu264@users.noreply.github.com>
2023-11-08 15:41:29 -0800

    Update Mission 5.py

--- Mission 5.py
+++ Mission 5.py
@@ -1,13 +1,17 @@
-import cv2
 from djitellopy import Tello
+import cv2
 
 tello = Tello()
 tello.connect()
-
+#tello.takeoff()
 tello.streamon()
-frame_read = tello.get_frame_read()
-
-tello.takeoff()
-cv2.imwrite("picture.png", frame_read.frame)
-
-tello.land()
+count = 0
+vidcap = cv2.VideoCapture(tello.get_udp_video_address())
+success,image = vidcap.read()
+success = True
+while success:
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))
+    success,image = vidcap.read()
+    print ('Read a new frame: ', success)
+    cv2.imwrite("./img/frame%d.jpg" % count, image) # save frame as JPEG file
+    count = count + 5
